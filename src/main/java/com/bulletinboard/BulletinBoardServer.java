5a470817371055228783d9014790fb2468dafdc0
aSempruch <32146438+aSempruch@users.noreply.github.com>
2018-09-26 14:19:14 -0400

    Initial Project Files

---
+++ src/main/java/com/bulletinboard/BulletinBoardServer.java
@@ -0,0 +1,10 @@
+package com.bulletinboard;
+
+public class BulletinBoardServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/bulletinboard/BulletinBoardService.java
@@ -0,0 +1,5 @@
+package com.bulletinboard;
+
+public class BulletinBoardService {
+
+}
